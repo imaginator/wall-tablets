96ef9211d22a99ec5324002ec95978441d693819
Simon Tennant <simon@imaginator.com>
2023-07-10 10:35:30 +0200

    try to stabilise wifi

--- push_settings.sh
+++ push_settings.sh
@@ -4,24 +4,26 @@ set -e
 # if $1 is not empty, use it as the rooms list
 if [ -z "$1" ]
 then
-  rooms="study lounge hallwaylarge bathroomguest bedroom1 bedroom2 bedroom3 kitchen bathroommain"
+  rooms="study lounge hallwaylarge bedroom1 bedroom2 bedroom3 kitchen bathroommain bathroomguest"
 else
   rooms=$1
 fi
 
 for room in $rooms; do
-  echo "room todo: $room"
+  echo "todo: $room"
 done
 
 for room in $rooms; do
-  if ping -c 1 -W 1 tablet-$room.imagilan &> /dev/null
+  if ping -c 2 -W 2 tablet-$room.imagilan &> /dev/null
   then
-    echo "doing room: $room"
     adb connect tablet-$room.imagilan &> /dev/null
     adb -s tablet-$room.imagilan push templates/fully-auto-settings.json /sdcard/fully-auto-settings.json &> /dev/null
     adb -s tablet-$room.imagilan shell pm grant com.fullykiosk.emm android.permission.WRITE_SECURE_SETTINGS # remove the system bars
+    adb -s tablet-$room.imagilan shell settings put global network_avoid_bad_wifi 0 # we only use one AP, if that's down, keep trying.
+    adb -s tablet-$room.imagilan shell settings put global wifi_scan_throttle 0 # don't throttle wifi scans
     adb -s tablet-$room.imagilan reboot &
-    sleep 2
+    sleep 1
+    echo "done: $room"
     adb -s tablet-$room.imagilan disconnect &> /dev/null
   fi
 done
